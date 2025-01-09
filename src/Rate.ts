89aeccabb8bee4bfb84f07bb7639104109dcc44b
Joon2000 <najoonwon22@gmail.com>
2025-01-09 22:25:46 +0900

    TEmp

--- src/Rate.ts
+++ src/Rate.ts
@@ -7,5 +7,13 @@ export class Rate extends SmartContract {
   init() {
     super.init();
     this.rate.set(Field(0));
+    this.participantNum.set(Field(0));
+  }
+
+  @method async update(newRate: Field, finalRate: Field) {
+    const currentRate = this.rate.get();
+    this.rate.requireEquals(currentRate);
+    const currentParticipantNum = this.participantNum.get();
+    this.participantNum.requireEquals(currentParticipantNum);
   }
 }
